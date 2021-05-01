688dd92835ba4fe18837121225cbd61c42c7374c
Arthur Eudeline <arthur.eudeline@outlook.com>
2021-05-01 13:24:06 +0200

    Initial Commit

---
+++ Shared/iOS_CrossPlatform_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_CrossPlatform_TestApp.swift
+//  Shared
+//
+//  Created by Arthur Eudeline on 01/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct iOS_CrossPlatform_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
